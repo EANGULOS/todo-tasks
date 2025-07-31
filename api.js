d5659345f799d5d5710a001cf78c470d3ec9abc3
Edward <edwardandres.10@hotmail.com>
2025-07-31 16:25:06 -0500

    se agrega carpeta task_schema

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 var express = require("express");
+var TaskModel = require('./Task_schema');
 var router = express.Router();
+
 
 let environment = null;
 
@@ -29,6 +31,29 @@ mongoose.connect(db, {
     console.log('Conexión a la base de datos exitosa');
 }).catch((err) => {
     console.error('Error al conectar a la base de datos', err);
+});
+
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+
+    var newTask = new TaskModel(task);
+
+    newTask.save()
+        .then(data => {
+            res.status(200).send("OK\n");
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        });
 });
 
 module.exports = router;
